a3857e0e65723e2a7a3e4c6ea2482a72980e8e60
Alejandro Cruz <canarygo1@gmail.com>
2019-03-18 23:52:23 +0000

    more

--- Github_ets/src/github_ets/Github_ets.java
+++ Github_ets/src/github_ets/Github_ets.java
@@ -15,7 +15,7 @@ public class Github_ets {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hola soy alejandro 2");
     }
     
 }
