ef78c39874c80ebc1e98bee1eee438820bf5181d
saulbkn <47863239+saulbkn@users.noreply.github.com>
2019-03-19 00:09:45 +0000

    Declaración de variables

--- Github_ets/src/github_ets/Github_ets.java
+++ Github_ets/src/github_ets/Github_ets.java
@@ -5,6 +5,8 @@
  */
 package github_ets;
 
+import java.util.Scanner;
+
 /**
  *
  * @author alejandrocruz
@@ -16,6 +18,21 @@ public class Github_ets {
      */
     public static void main(String[] args) {
         System.out.println("hola soy alejandro 3");
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Tomar un entero: ");
+        int valor = input.nextInt();
+        String binario = "";
+
+        int divisor = 20;
+        while(divisor != 0){
+            int resto = valor%2;
+            binario = resto + binario;
+            divisor = valor/2;
+            valor = valor/2;
+        }
+
+        System.out.print(binario);
     }
     
 }
